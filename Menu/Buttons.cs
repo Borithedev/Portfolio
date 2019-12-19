53c060b5dcf8cb49b8770447681ac013435317fb
Bori Elijah Esuruoso <59065427+Borithedev@users.noreply.github.com>
2019-12-19 19:53:14 +0000

    Add files via upload

---
+++ Menu/Buttons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Buttons : MonoBehaviour
+{
+    public Button menubuttons;
+    public Sprite unselected;
+    public Sprite selected;
+    private int counter = 0;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        menubuttons = GetComponent<Button>();
+    }
+
+        public void changeButton()
+        {
+            counter++;
+            if(counter % 2 == 0)
+            {
+                menubuttons.image.overrideSprite = unselected;
+            }
+            else
+            {
+                menubuttons.image.overrideSprite = selected;
+            }
+        }
+}
---
+++ Menu/StreamVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage rawimage;
+    public VideoPlayer videoplayer;
+    public AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Playvideo());
+    }
+    IEnumerator Playvideo()
+    {
+        videoplayer.Prepare();
+        WaitForSeconds WaitForSeconds = new WaitForSeconds(1);
+        while(!videoplayer.isPrepared)
+        {
+            yield return WaitForSeconds;
+            break;
+        }
+        rawimage.texture = videoplayer.texture;
+        videoplayer.Play();
+        audiosource.Play();
+    }
+}
